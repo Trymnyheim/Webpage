da4ba6934ee2ef8bbe92695659fc51db355b44a3
Trymnyheim <trym.haakon.nyheim@gmail.com>
2024-12-30 01:12:06 +0100

    Improved management of tasks

--- web/react/src/components/NavigationBar.jsx
+++ web/react/src/components/NavigationBar.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function NavigationBar() {
-  return (
-    <Navbar bg="light" data-bs-theme="light">
-        <Container>
-            <Navbar.Brand as={Link} to="/home">Trym H. Nyheim</Navbar.Brand> {/* Use Link for Brand */}
-            <Nav className="me-auto">
-                <Nav.Link as={Link} to="/home">Home</Nav.Link>
-                <Nav.Link as={Link} to="/tasks">Tasks</Nav.Link>
-                <Nav.Link as={Link} to="/games">Games</Nav.Link>
-            </Nav>
-        </Container>
-    </Navbar>
-  );
+  	return (
+		<Navbar expand="lg" className="bg-body-tertiary">
+			<Container>
+				<Navbar.Brand as={Link} to="/home">Trym H. Nyheim</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="me-auto">
+						<Nav.Link as={Link} to="/home">Home</Nav.Link>
+						<Nav.Link as={Link} to="/tasks">Tasks</Nav.Link>
+						<Nav.Link as={Link} to="/games">Games</Nav.Link>
+						<NavDropdown title="Dropdown" id="basic-nav-dropdown">
+							<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+							<NavDropdown.Item href="#action/3.2">
+								Another action
+							</NavDropdown.Item>
+							<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+							<NavDropdown.Divider />
+							<NavDropdown.Item href="#action/3.4">
+								Separated link
+							</NavDropdown.Item>
+						</NavDropdown>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+  	)
 }
 
 export default NavigationBar;
--- web/react/src/components/tasks/TaskList.jsx
+++ web/react/src/components/tasks/TaskList.jsx
@@ -1,46 +1,20 @@
 import Task from './Task'
 import { useState, useEffect } from 'react';
-import './Task.css';
 
 
 function TaskList(props) {
-    const [tasks, setTasks] = useState([]);
+    const [taskList, setTaskList] = useState(props.taskList);
+    
     useEffect(() => {
-        const fetchTasks = async () => {
-            try {
-                const res = await fetch('http://localhost:3000/api/tasks/gettasks')
-                if (!res.ok) {
-                    throw new Error();
-                }
-                const data = await res.json()   
-                setTasks(data)
-            } catch (error) {
-                console.log('Unable to fetch tasks: ', error)
-                setTasks(testData())
-            }
-        }
-        fetchTasks();
-    }, [])
-
-    function testData() {
-        return [
-            {id: 1, title: "Test Nr 1", desc: "Dette er den første testtasken. Vi får se hvordan dette går da!", date: "21.10.97", comp: 1},
-            {id: 2, title: "Test2", desc: "Andre test", date: "31.10.97", comp: 0}
-        ]
-    }
+        setTaskList(props.taskList);
+    }, [props.taskList]);
 
     function updateTask(id, title, desc, date, comp) {
         try {
             // Send request to server
             
             // Update in client
-            const index = tasks.findIndex((task) => task.id === id);
-            if (index === -1)
-                throw new Error("Task not found.");
-            const updatedTasks = [...tasks];
-            updatedTasks[index] = { ...updatedTasks[index], title, desc, date, comp };
 
-            setTasks(updatedTasks);
             console.log(`Updated task with ID ${id}.`);
         } catch (error) {
             // Show error message to user!
@@ -51,7 +25,6 @@ function TaskList(props) {
     function deleteTask(id) {
         try {
             // Send request to server
-            setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id));
             console.log(`Deleted task with ID ${id}.`)
         } catch (error) {
             // Show error message to user!
@@ -61,9 +34,9 @@ function TaskList(props) {
 
     return (
         <>
-            <h2>{props.id}</h2>
+            <h2>{taskList.title}</h2>
             <div className="taskList">
-                {tasks.map(task => 
+                {taskList.tasks.map(task => 
                     <Task key = {task.id} id = {task.id} title = {task.title} desc = {task.desc} date = {task.date} comp = {task.comp} update = {updateTask} delete = {deleteTask} />
                 )}
             </div>
