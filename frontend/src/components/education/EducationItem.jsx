fccf0b55454826eaf0e66b8e1d6a0365e39c0ba7
Trymnyheim <trym.haakon.nyheim@gmail.com>
2025-07-12 16:17:02 +0200

    add: styling and adjustments

--- frontend/src/components/education/EducationItem.jsx
+++ frontend/src/components/education/EducationItem.jsx
@@ -1,11 +1,55 @@
+import './education.css';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
 
-function EducationItem({educationItem}) {
+function EducationItem({educationItem, t}) {
 
     return (
-        <div className="education-item" style={{marginBottom: '96px', marginLeft: '96px'}}>
-            <img src={educationItem.institution.logo} style={{backgroundColor: 'lightgray', padding: '24px', height: '100px'}} />
-            <p>{educationItem.degree}</p>
-            <p>{educationItem.level}</p>
+        <div className="education-item center" >
+            <Table striped bordered hover>
+                <tbody>
+                    <tr>
+                        <td>
+                            {educationItem.degree}
+                            <br/>
+                            <Button href={educationItem.link} variant="secondary" className="margin-sm">
+                                {t('general.about')}
+                            </Button>
+                        </td>
+                        <td>
+                            {educationItem.level}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colSpan={2} style={{backgroundColor: 'gray'}}>
+                            <div className="institution-container">
+                                <Button href={educationItem.institution.path} variant="light">{educationItem.institution.name}</Button>
+                                <img
+                                    src={educationItem.institution.logo}
+                                    alt={educationItem.institution.name}
+                                    href={educationItem.institution.path}
+                                />
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colSpan={2}>
+                            <span>{educationItem.start}</span>
+                            <span className="margin">–</span>
+                            <span>{educationItem.end || 'Present'}</span>
+                        </td>
+
+                    </tr>
+                    {educationItem.exchange?.institution &&
+                        <tr>
+                            <td colSpan={2}>
+                                {`${t('general.exchangeTo')} ${educationItem.exchange.institution} (${educationItem.exchange.semesters} semester)`}
+                            </td>
+                        </tr>
+                    }
+                </tbody>
+            </Table>
+            <p></p>
         </div>
     )
 }
