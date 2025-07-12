898ca9f0d3c1211363c8fc95ca49b20c84222156
Trymnyheim <trym.haakon.nyheim@gmail.com>
2025-07-12 16:28:06 +0200

    fix: Error in app wrapper

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,14 +6,14 @@ import Home from './pages/Home.jsx';
 
 function App() {
   return (
-        <div className="body-content">
-            <Router>
-                <NavigationBar />
+        <Router>
+            <NavigationBar />
+            <div className="body-content">
                 <Routes>
                     <Route path="/" element={<Home />} />
                 </Routes>
-            </Router>
-        </div>
+            </div>
+        </Router>   
     )
 }
 
