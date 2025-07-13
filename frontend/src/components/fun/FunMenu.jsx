a9be12e00cd95de3435333b17703cdcac66d5cb4
Trymnyheim <trym.haakon.nyheim@gmail.com>
2025-07-13 18:46:21 +0200

    add: close when click outside menu

--- frontend/src/components/fun/FunMenu.jsx
+++ frontend/src/components/fun/FunMenu.jsx
@@ -1,51 +1,67 @@
 import { Link } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
-function FunMenu({funItems, isOverlay}) {
-    
+function FunMenu({ funItems, isOverlay }) {
     const [show, setShow] = useState(false);
+    const menuRef = useRef(null);
 
-    const toogleShow = () => {
-        if (isOverlay)
-        setShow(!show);
-    }
+    const toggleShow = () => {
+        if (isOverlay) setShow(!show);
+    };
+
+    // Close overlay if click outside fun-menu
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (
+                menuRef.current &&
+                !menuRef.current.contains(event.target) &&
+                show
+            ) {
+                setShow(false);
+            }
+        }
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [show]);
 
     const back = {
         title: 'Back to Fun',
         img: '/imgs/fun/back.svg',
-        path: '/fun'
-    }
+        path: '/fun',
+    };
 
     return (
-        <div onClick={toogleShow} className={`fun-menu ${isOverlay ? "fun-overlay-active" : ""}`}>
-            {isOverlay &&
-            <Button variant="success">Fun Menu</Button>
-            }
-            <div className={`fun-card-container ${show ? "fun-overlay" : ""}`}>
-                {isOverlay &&
-                    <FunCard funItem={back} isBack/>
-                }
+        <div
+            ref={menuRef}
+            onClick={toggleShow}
+            className={`fun-menu ${isOverlay ? 'fun-overlay-active' : ''}`}
+        >
+            {isOverlay && <Button variant="success">Fun Menu</Button>}
+            <div className={`fun-card-container ${show ? 'fun-overlay' : ''}`}>
+                {isOverlay && <FunCard funItem={back} isBack />}
                 {funItems.map((funItem, index) => (
                     <FunCard key={index} funItem={funItem} />
                 ))}
             </div>
         </div>
-    )
+    );
 }
 
-function FunCard({funItem, isBack}) {
+function FunCard({ funItem, isBack }) {
     return (
         <Link to={funItem.path}>
             <Card className="fun-card center bg-success" style={{ width: '16rem' }}>
                 <div className="fun-card-title">
                     <Card.Title className="margin">{funItem.title}</Card.Title>
                 </div>
-                <Card.Img src={funItem.img} className={isBack ? 'back-img' : ''}/>
+                <Card.Img src={funItem.img} className={isBack ? 'back-img' : ''} />
             </Card>
         </Link>
-    )
+    );
 }
 
 export default FunMenu;
