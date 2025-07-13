fa86ccd1dc898ae23ae33ce669b541ff542c7658
Trymnyheim <trym.haakon.nyheim@gmail.com>
2025-07-14 00:07:25 +0200

    add: monty statistics

--- frontend/src/components/fun/MontyHall.js
+++ frontend/src/components/fun/MontyHall.js
@@ -1,6 +1,7 @@
 class MontyHall {
     constructor() {
-        this.doors = this.getDoors()
+        this.doors = [];
+        this.statistics = undefined;
     }
 
     getGoat(selection) {
@@ -11,35 +12,98 @@ class MontyHall {
         const doors = [false, false, false];
         const car = this.random(3);
         doors[car] = true;
-        return doors;
+        this.doors = doors;
     }
     
     random(x) {
         return Math.floor(Math.random() * x);
     }
 
-/*
-    playMonty(shouldSwitch) {
-        let selection = random(3);
-        const goatIndex = this.getGoat(this.doors, selection);
-        if (shouldSwitch) {
-            selection = [0, 1, 2].find((n) => n !== selection && n !== goatIndex);
-        }
-        return doors[selection]
+    generateStatistics(x) {
+        const [switchSuccess, switchFail] = this.testMonty(x, true);
+        const [withoutSuccess, withoutFail] = this.testMonty(x, false);
+
+        const d = x/100 // Percentage denominator
+
+        this.statistics = this.getStatJSON(switchSuccess, switchSuccess/d,
+            switchFail, switchFail/d, withoutSuccess, withoutSuccess/d,
+            withoutFail, withoutFail/d);
     }
 
     testMonty(amount, shouldSwitch) {
         let success = 0;
-        let fail = 0;
         for (let i = 0; i < amount; i++) {
-            if (playMonty(shouldSwitch))
+            if (this.playMonty(shouldSwitch))
                 success++;
-            else
-                fail++;
         }
-        console.log(`Successes: ${success}\nFails: ${fail}`);
+        return [success, amount - success];
     }
-*/
+
+    playMonty(shouldSwitch) {
+        let selection = this.random(3);
+        this.getDoors();
+        const goatIndex = this.getGoat(selection);
+        if (shouldSwitch) {
+            selection = [0, 1, 2].find((n) => n !== selection && n !== goatIndex);
+        }
+        return this.doors[selection]
+    }
+
+    getStatJSON(ssn, ssp, sfn, sfp, wsn, wsp, wfn, wfp) {
+        return {
+            'switch': {
+                'success': {
+                    'number': ssn,
+                    'percent': ssp
+                },
+                'fail': {
+                    'number': sfn,
+                    'percent': sfp
+                },
+                'total': ssn + sfn
+            },
+            'without': {
+                'success': {
+                    'number': wsn,
+                    'percent': wsp
+                },
+                'fail': {
+                    'number': wfn,
+                    'percent': wfp
+                },
+                'total': wsn + wfn
+            }
+        }
+    }
 }
+
+
+
+/*
+
+'switch': {
+    'success': {
+        'number': 68,
+        'percent': 68
+    },
+    'fail': {
+        'number': 32,
+        'percent': 32
+    },
+    'total': 100
+},
+'without': {
+    'success': {
+        'number': 62,
+        'percent': 31
+    },
+    'fail': {
+        'number': 138,
+        'percent': 70
+    },
+    'total': 200
+}
+
+*/
 
 export default MontyHall;
