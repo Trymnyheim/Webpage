4b1744808e983988711cc9289a6f82897704af10
Trymnyheim <trym.haakon.nyheim@gmail.com>
2025-07-22 13:25:25 +0200

    fix: css selected language

--- frontend/src/components/LanguageSwitcher.jsx
+++ frontend/src/components/LanguageSwitcher.jsx
@@ -1,26 +1,25 @@
-import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 function LanguageSwitcher() {
   	const { i18n } = useTranslation();
 
   	const changeLanguage = (language) => {
-    	i18n.changeLanguage(language); // Switches language
-  	};
-
-  	const flagStyle = {
-		height: "28px",
-		width: "40px",
-		marginRight: "8px",
-		borderRadius: "8px",
-		cursor: "pointer"
+    	i18n.changeLanguage(language);
   	}
 
   	return (
-		<div>
-			<img src="/imgs/en_flag.png" onClick={() => changeLanguage("en")} style={flagStyle} />
-			<img src="/imgs/no_flag.png" onClick={() => changeLanguage("no")}style={flagStyle} />
-		</div>
+		<>
+			<img 
+				src="/imgs/en_flag.png"
+				onClick={() => changeLanguage("en")} 
+				className={`flag${i18n.language === 'en' ? ' selected' : ''}`}
+			/>
+			<img
+				src="/imgs/no_flag.png"
+				onClick={() => changeLanguage("no")} 
+				className={`flag${i18n.language === 'no' ? ' selected' : ''}`}
+			/>
+		</>
   	);
 }
 
