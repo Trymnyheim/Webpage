2f5162173e5fa264eea01257f294e6a0d24a5b18
trymhnyheim <trym.haakon.nyheim@gmail.com>
2025-07-28 13:05:01 +0200

    fix: port nr

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 3001;
-const host = 'localhost';
+const port = 3000;
+const host = '0.0.0.0';
 
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: 'https://trymhnyheim.no' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
